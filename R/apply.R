f9b0994910153a8a65d26fd40994baf7dd6cdfd8
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-12 17:19:55 -0700

    the substitute(substitute approach seems to be working

--- R/apply.R
+++ R/apply.R
@@ -2,9 +2,17 @@
 nworkers = 2
 
 
-#' Convert \code{apply} To parallel
+# From Hadley Wickham's pryr / Advanced R
+sub_expr <- function(expr, env) {
+    stopifnot(is.language(expr))
+    call <- substitute(substitute(expr, env), list(expr = expr))
+    eval(call)
+}
+
+
+#' Convert \code{apply} To Parallel
 #'
-#' Transform the top level of a single 
+#' Transform a single
 #' \code{apply()} function call into a parallel version
 #'
 #' @param expression \code{apply()} function call
@@ -16,18 +24,34 @@ apply_parallel = function(incode)
     if(incode[[3]] != 2)
        stop("Only considering explicit MARGIN = 2 at the moment")
 
-    # Split initial data
-    firstpart = expression(
-        n <- ncol(X)
-        , idx <- parallel::splitIndices(n, nworkers)
+    template = parse(text = "
+        n = ncol(X)
+        idx = parallel::splitIndices(n, nworkers)
+        parts = parallel::mclapply(idx, function(columns){
+            APPLY_X_CHUNK
+        })
+
+        # TODO: handle general case. This assumes a scalar result
+        unlist(parts)
+    ")
+
+    # The first argument to apply
+    Xcode = incode[[2]]
+    Xstring = deparse(Xcode)
+
+    # Transform incode to a form chunked on columns
+    # Essentially X becomes X[, columns]
+    chunk_name_map = list(
+        sub_expr(quote(X[, columns]), list(X = Xcode))
     )
+    # Not sure if setting name to arbitrary code will always work
+    names(chunk_name_map) = Xstring
+    APPLY_X_CHUNK = sub_expr(incode, chunk_name_map)
 
 
-
-    # Evaluate in parallel
-
-    # Recombine result
-
+    sub_expr(template, list(X = Xcode
+                            , APPLY_X_CHUNK = APPLY_X_CHUNK
+                            ))
 
 }
 
