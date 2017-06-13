3f45138abdbc1328112230f95bf2dd5bfff33c80
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-13 12:07:45 -0700

    found the apply locations

--- R/apply.R
+++ R/apply.R
@@ -2,20 +2,20 @@
 #'
 #' \code{apply(X, 2, FUN)} becomes \code{apply(X[, columns], 2, FUN)}
 #'
-#' @param code where \code{apply} is \code{code[[1]]}
-#' @return outcode for use in chunked columns
-apply_column_chunk = function(code)
+#' @param expr where \code{apply} is \code{expr[[1]]}
+#' @return outexpr for use in chunked columns
+apply_column_chunk = function(expr)
 {
     # The first argument to apply
-    Xcode = code[[2]]
-    Xstring = deparse(Xcode)
+    Xexpr = expr[[2]]
+    Xstring = deparse(Xexpr)
 
     chunk_name_map = list(
-        sub_expr(quote(X[, columns, drop = FALSE]), list(X = Xcode))
+        sub_expr(quote(X[, columns, drop = FALSE]), list(X = Xexpr))
     )
     # Not sure if setting name to arbitrary code will always work
     names(chunk_name_map) = Xstring
-    sub_expr(code, chunk_name_map)
+    sub_expr(expr, chunk_name_map)
 }
 
 
@@ -27,10 +27,10 @@ apply_column_chunk = function(code)
 #' @param expression \code{apply()} function call
 #' @return modified parallel code
 #' @export
-apply_parallel = function(code)
+apply_parallel = function(expr)
 {
 
-    if(code[[3]] != 2)
+    if(expr[[3]] != 2)
        stop("Only considering explicit MARGIN = 2 at the moment")
 
     template = parse(text = "
@@ -45,10 +45,37 @@ apply_parallel = function(code)
         unlist(parts)
     ")
 
-    sub_expr(template, list(X = code[[2]]
-                            , APPLY_COLUMN_CHUNK = apply_column_chunk(code)
+    sub_expr(template, list(X = expr[[2]]
+                            , APPLY_COLUMN_CHUNK = apply_column_chunk(expr)
                             , NWORKERS = nworkers()
                             ))
+}
+
+
+#' Find Location Of Apply In Parse Tree
+#'
+#' Only looks at the top level expression.
+#'
+#' @param expr expression
+#' @param apply_func vector of apply names
+#' @return integer 1 for \code{apply(x, ...}, 3 for \code{y <- apply(x,
+#'      ...}, 0 otherwise
+apply_location = function(expr, apply_func = "apply")
+{
+    # A single token on its own line
+    if(length(expr) == 1)
+        return(0L)
+
+    e1 = deparse(expr[[1]])  
+
+    if(e1 %in% apply_func)
+        return(1L)
+
+    assigners = c("<-", "=", "assign")
+    if(e1 %in% assigners)
+        if(deparse(expr[[3]][[1]]) %in% apply_func)
+            return(3L)
+    0L
 }
 
 
@@ -56,12 +83,10 @@ apply_parallel = function(code)
 #'
 #' Only looks at the top level expression.
 #'
-#' @param code expression
+#' @param expr expression
 #' @return logical locations of apply
 #' @export
-find_apply = function(code)
+find_apply = function(expr)
 {
-
-    toplevel = sapply(code, `[[`, 1)
-
+    sapply(expr, apply_location) != 0L
 }
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,12 +1,16 @@
+library(autoparallel)
+
 test_that("find apply calls inside script", {
 
-    code = parse(text = "
+    expr = parse(text = "
         x = matrix(1:10, ncol = 2)
+        x
         colmaxs = apply(x, 2, max)
-        colmaxs_ <- apply(x, 2, max)
+        colmaxs2 <- apply(x, 2, max)
+        assign('colmaxs3', apply(x, 2, max))
         apply(x, 2, min)
     ")
 
-    expect_equal(find_apply, c(FALSE, TRUE, TRUE))
+    expect_equal(find_apply(expr), c(FALSE, FALSE, TRUE, TRUE, TRUE, TRUE))
 
 })
