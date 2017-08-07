6cfb1e0ab4c293e25e98182e47bd8b3d5f7fd62c
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-07 15:03:19 -0700

    will do the Rd files later, failing test for global vars

--- tests/testthat/test_parallelize.R
+++ tests/testthat/test_parallelize.R
@@ -1,11 +1,11 @@
 context("parallelize")
 
+# parallelize works off variables in the global environment
+# testthat does some other things here.
+assign("x", list(letters, LETTERS, 1:10), envir = .GlobalEnv)
+
 
 test_that("basics with default", {
-
-    # parallelize works off variables in the global environment
-    # testthat does some other things here.
-    assign("x", list(letters, LETTERS, 1:10), envir = .GlobalEnv)
 
     do = parallelize(x)
     actual = do(lapply(x, head))
@@ -18,4 +18,16 @@ test_that("basics with default", {
     actual = do(lapply(x, head2))
 
     expect_identical(actual, lapply(x, head2))
+})
+
+
+test_that("finds global variables", {
+
+    assign("n", 10, envir = .GlobalEnv)
+
+    do = parallelize(x, spec = 2L)
+    actual = do(n)
+
+    expect_identical(actual, c(n, n))
+
 })
