4d80e44133be7b9bcdfb7c86da5fae6f0f52fc9e
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-23 15:25:56 -0700

    signatures for tuning funcs

---
+++ R/tune.R
@@ -0,0 +1,30 @@
+#' Tune Function For Specific Arguments
+#'
+#' Return a modified version of the input function 
+#'
+#' @export
+#' @param FUN function to be tuned
+#' @param ... arguments to FUN that are being tuned
+#' @examples
+#' # t is the tuning parameter
+#' f = function(x, t){
+#'  if(x > 0) Sys.sleep(t^2)
+#'  x
+#' }
+#' # Suppose we plan to call this many times with positive x values
+#' f2 = tune(f, x = 100, t = tune_param(list(-0.05, 0.01, 0.1)))
+#' # f2 should now have 0.01 as the default argument for t
+tune = function(FUN, ...)
+{
+}
+
+
+#' Mark Parameter For Tuning
+#'
+#' @param x list of arguments to time
+#' @export
+tune_param = function(x)
+{
+    attr(x, "tune") = TRUE
+    x
+}
