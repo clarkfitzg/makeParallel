b6b8c7dbc454f046f1d132f6ceb1de6c72d4c0c3
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-29 15:27:48 -0700

    the naive version continues to be fast, darn it.

---
+++ R/retrofit.R
@@ -0,0 +1,8 @@
+#' Create a Function That Learns To Make Itself Faster
+#' 
+#' @param ... functions, possibly with several implementations
+#' @return learning function
+#' @export
+learn_func = function (...)
+{
+}
