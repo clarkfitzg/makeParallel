96158bc3aa8c53bcdc95325c4cf306a9fcd28b0f
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-30 14:06:47 -0700

    API for evolve

--- R/evolve.R
+++ R/evolve.R
@@ -1,10 +1,35 @@
 #' Create Function That Learns To Make Itself Faster
 #' 
-#' @param ... functions performing the same operation but with different
-#'      implementations.
+#' This assumes that all implementations can share the same model for complexity. For
+#' example, computing the mean requires O(n) operations.
+#' 
+#' Each different implementation gets a model to 
+#' 
+#' @param func reference implementation of the function to be evolved
+#' @param ... different implementations of the reference function
+#' @param complexity function to be called with the same arguments as the
+#'  implementation functions. Should return a vector which can be converted
+#'  to numeric.
+#' @param model statistical model of time as a noisy function of complexity.
 #' @return evolving function
 #' @export
-evolve = function (...)
+evolve = function (func, ..., complexity = nrow_first_arg, model = "updating_lm")
 {
-    implementations = list(...)
+    funcs = c(list(func), list(...))
+
+    times = data.frame(func = integer(), time = numeric())
+
+}
+
+
+#' Count the number of rows in the first argument
+nrow_first_arg = function (...)
+{
+    nrow(list(...)[[1]])
+}
+
+
+#' Create an updating linear model
+updating_lm = function ()
+{
 }
