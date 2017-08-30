ee5a560cd906048d21bfed43c87d0c921d4cd73a
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-30 10:00:24 -0700

    thinking about evolve func

---
+++ R/evolve.R
@@ -0,0 +1,10 @@
+#' Create a Function That Learns To Make Itself Faster
+#' 
+#' @param ... functions, performing the same operation but with different
+#'      implementations.
+#' @return evolving function
+#' @export
+evolve = function (...)
+{
+    implementations = list(...)
+}
--- R/retrofit.R
+++
@@ -1,8 +0,0 @@
-#' Create a Function That Learns To Make Itself Faster
-#' 
-#' @param ... functions, possibly with several implementations
-#' @return learning function
-#' @export
-learn_func = function (...)
-{
-}
