50365cc6dd6286e86eb233a6e19dae258b31498e
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-30 15:38:20 -0700

    tentative implementation of evolve func

--- R/evolve.R
+++ R/evolve.R
@@ -1,9 +1,10 @@
 #' Create Function That Learns To Make Itself Faster
 #' 
 #' This assumes that all implementations can share the same model for complexity. For
-#' example, computing the mean requires O(n) operations.
+#' example, computing the mean requires O(n) operations, so a linear model
+#' of the form time = a + bn is appropriate.
 #' 
-#' Each different implementation gets a model to 
+#' Each different implementation gets its own model to fit.
 #' 
 #' @param func reference implementation of the function to be evolved
 #' @param ... different implementations of the reference function
@@ -13,12 +14,31 @@
 #' @param model statistical model of time as a noisy function of complexity.
 #' @return evolving function
 #' @export
-evolve = function (func, ..., complexity = nrow_first_arg, model = "updating_lm")
+evolve = function (func, ..., complexity = nrow_first_arg, model = lm)
 {
     funcs = c(list(func), list(...))
 
-    times = data.frame(func = integer(), time = numeric())
+    # Start out with lists of NULL
+    models = vector(length(funcs), mode = "list")
+    timings = vector(length(funcs), mode = "list")
 
+    # TODO: Better design may be to separate this into separate functions
+    # that record how they are used.
+
+    function (...)
+    {
+        # For development
+        i = 1
+
+        t = microbenchmark::microbenchmark(out <- funcs[[i]](...), times = 1L)$time
+
+        # Record the observation that was just made
+        obs <- data.frame(time = t, complexity = complexity)
+        timings[[i]] <<- rbind(timings[[i]], obs)
+
+        # Update the corresponding model
+        models[[i]] <<- model(time ~ ., data = timings[[i]])
+    }
 }
 
 
@@ -26,10 +46,4 @@ evolve = function (func, ..., complexity = nrow_first_arg, model = "updating_lm"
 nrow_first_arg = function (...)
 {
     nrow(list(...)[[1]])
-}
-
-
-#' Create an updating linear model
-updating_lm = function ()
-{
 }
