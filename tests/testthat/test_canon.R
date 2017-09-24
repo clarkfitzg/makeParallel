f764c495e461e2ff9d62a72c14cc29f87abd3d4a
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-09-24 15:33:18 -0700

    test for equal, not identical

--- tests/testthat/test_canon.R
+++ tests/testthat/test_canon.R
@@ -4,32 +4,32 @@ test_that("names_to_ssb helper functions", {
 
     code = quote(dframe$b)
     transformed = dollar_to_ssb(code, letters)$statement
-    expect_identical(quote(dframe[, 2L]), transformed)
+    expect_equal(quote(dframe[, 2L]), transformed)
 
     code = quote(dframe[["b"]])
     transformed = double_to_ssb(code, letters)$statement
-    expect_identical(quote(dframe[, 2L]), transformed)
+    expect_equal(quote(dframe[, 2L]), transformed)
 
     code = quote(dframe[[2L]])
     transformed = double_to_ssb(code, letters)$statement
-    expect_identical(quote(dframe[, 2L]), transformed)
+    expect_equal(quote(dframe[, 2L]), transformed)
 
     code = quote(dframe[, "b"])
     transformed = single_to_ssb(code, letters)$statement
-    expect_identical(quote(dframe[, 2L]), transformed)
+    expect_equal(quote(dframe[, 2L]), transformed)
 
     code = quote(dframe[, c("b", "c")])
     transformed = single_to_ssb(code, letters)$statement
 
-    expect_identical(quote(dframe[, c(2L, 3L)]), transformed)
+    expect_equal(quote(dframe[, 2:3]), transformed)
 
-    code = quote(dframe[, c(2L, 3L)])
+    code = quote(dframe[, c(2L, 4L)])
     transformed = single_to_ssb(code, letters)$statement
-    expect_identical(quote(dframe[, c(2L, 3L)]), transformed)
+    expect_equal(quote(dframe[, c(2L, 4L)]), transformed)
 
     code = quote(dframe[condition, "b"])
     transformed = single_to_ssb(code, letters)$statement
-    expect_identical(quote(dframe[condition, 2L]), transformed)
+    expect_equal(quote(dframe[condition, 2L]), transformed)
 
 })
 
