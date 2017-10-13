d213758c8a4145d73e29229ecd11befb9a8214fc
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-13 10:59:28 -0700

    interactive vignette builds

--- vignettes/buildvignettes.R
+++ vignettes/buildvignettes.R
@@ -1,8 +1,5 @@
-rmarkdown::render("assumptions.Rmd", "html_document")
-
-
-rmarkdown::render("transpile.Rmd", "html_document")
-
+#rmarkdown::render("assumptions.Rmd", "html_document")
+#rmarkdown::render("transpile.Rmd", "html_document")
 
 
 Rmd = grep("Rmd", list.files(), value = TRUE)
