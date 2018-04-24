c3522a6d65aed9b0ec43bea13e0bc6bc8aac4203
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-24 13:14:40 -0700

    starting to document autoparallel() function

---
+++ R/autoparallel.R
@@ -0,0 +1,16 @@
+#' Convert Code Into Parallel
+#'
+#' Transforms code into a parallel form.
+#'
+#' @export
+#' @param code file name, expression from \code{\link[base]{parse()}}
+#' @param runfirst logical, evaluate the code once to gather timings?
+#' @param ... additional arguments to 
+#' @return code that will execute in parallel if possible
+#' @examples
+#' autoparallel("my_slow_serial.R")
+#' pcode = autoparallel(parse(text = "lapply(x, f)"))
+autoparallel = function(code, ...)
+{
+    UseMethod("autoparallel")
+}
