412af803d79d1f84cceeb0be77994f1a8957544a
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-05-24 09:59:08 -0700

    run_and_measure() looks reasonable

---
+++ R/run_and_measure.R
@@ -0,0 +1,38 @@
+#' Run the code in the task graph and measure how long each expression
+#' takes to run as well as the object sizes of each variable that can
+#' possibly be transferred.
+#'
+#' @param timer function that returns a timestamp, try using
+#'  microbenchmark::get_nanotime here for more precision.
+#' @param envir environment to evaluate the code in.
+run_and_measure = function(taskgraph, envir = globalenv(), timer = Sys.time)
+{
+    tg = taskgraph$task_graph
+    code = taskgraph$input_code
+    n = length(code)
+    times = numeric(n)
+
+    # Eliminate as much overhead for the timing as possible.
+    force(envir)
+
+    for(i in seq(n)){
+        # This is additional overhead beyond
+        # microbenchmark::microbenchmark, so we lose the fine precision, but
+        # as long as we have millisecond resolution we'll be fine.
+        gc()
+        start_time = timer()
+        eval(code[[i]], envir)
+        end_time = timer()
+        times[i] = end_time - start_time
+
+        from_rows = tg$type == "use-def" & tg$from == i
+        vars_to_measure = tg[from_rows, "value"]
+        for(v in vars_to_measure){
+            size = object.size(get(v, envir))
+            tg[from_rows & tg$value == "v", "size"] = size
+        }
+    }
+    taskgraph$task_graph = tg
+    taskgraph$times = as.numeric(times)
+    taskgraph
+}
