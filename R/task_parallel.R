edf28aeb4d1c3eda316542e0f0e7a99b77f0cdef
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-06-07 13:09:26 -0700

    midway through generalizing parts of code pipeline to handle both types of transformations

--- R/task_parallel.R
+++ R/task_parallel.R
@@ -10,9 +10,12 @@
 #'
 #' @export
 #' @param code file name, expression from \code{\link[base]{parse}}
+#' @param taskgraph result of \code{\link{task_graph}}
 #' @param runfirst logical, evaluate the code once to gather timings?
 #' @param ..., additional arguments to scheduler
 #' @param gen_script_prefix character added to front of file name
+#' @param output_file where to write the generated script, or NULL to not
+#'  write it.
 #' @return list of output from each step
 #' @examples
 #' \dontrun{
@@ -22,19 +25,27 @@
 #' y = rep(1, 100)
 #' z = x + y"))
 task_parallel = function(code
+    , taskgraph = task_graph(code)
     , runfirst = FALSE
     , scheduler = min_start_time
     , code_generator = gen_socket_code
     , ...
 #    , code_generator_args = list()
     , gen_script_prefix = "gen_"
+    , output_file = NULL
     )
 {
-    taskgraph = task_graph(code)
     if(runfirst) taskgraph = run_and_measure(taskgraph)
     schedule = scheduler(taskgraph, ...)
     out = code_generator(schedule)
+    finish_code_pipeline(out, output_file)
+    out
+}
 
+finish_code_pipeline = function(generated, output_file)
+{
+    if(!is.null(output_file) || !is.null(generated$input_file))
+        # TODO: Come back to this point
     if(is.character(code)){
         # It's a file name
         gen_file_name = file.path(dirname(code), paste0(gen_script_prefix, basename(code)))
@@ -42,5 +53,4 @@ task_parallel = function(code
         message(sprintf("generated parallel code is in %s", gen_file_name))
         out[["gen_file_name"]] = gen_file_name
     }
-    out
 }
