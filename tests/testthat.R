56a483ed6e31b1174d551a7984627fc3787ce057
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-07-02 17:48:02 -0700

    almost there, only problem is why doesnt testthat run for R cmd check?

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
 library(testthat)
 # https://github.com/r-lib/testthat/issues/86
-Sys.setenv("R_TESTS" = "")
+#Sys.setenv("R_TESTS" = "")
 library(makeParallel)
 
 # Allows parse() to check equality between expressions
