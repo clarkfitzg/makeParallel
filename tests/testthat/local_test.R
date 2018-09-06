824c7efbbad9445fded21a6c10996f5a55218256
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-09-06 12:18:41 -0700

    no need to actually skip tests

--- tests/testthat/local_test.R
+++ tests/testthat/local_test.R
@@ -1,10 +1,6 @@
 # Tests that should not run on CRAN.
-# This includes the plotting that requires the command line graphviz (dot)
-# install.
 
 test_that("depend graph plotting through command line graphviz (dot)", {
-
-skip("Requires dot program to be installed.")
 
 g = inferGraph("ex.R")
 #f = tempfile(pattern = "plot", fileext = ".pdf")
