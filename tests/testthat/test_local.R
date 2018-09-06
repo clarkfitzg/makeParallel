e6d3356c1f0c1e8bbe1aaeec92d724d7984b4743
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-09-06 12:15:50 -0700

    skip dot test

--- tests/testthat/test_local.R
+++ tests/testthat/test_local.R
@@ -2,7 +2,9 @@
 # This includes the plotting that requires the command line graphviz (dot)
 # install.
 
-test_that("descendant removal", {
+test_that("depend graph plotting through command line graphviz (dot)", {
+
+skip("Requires dot program to be installed.")
 
 g = inferGraph("ex.R")
 #f = tempfile(pattern = "plot", fileext = ".pdf")
