7e67d3ca7281b762f74b5c35e7902583639fea81
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-09-13 11:43:47 -0700

    starting with fork algo

--- R/scheduleFork.R
+++ R/scheduleFork.R
@@ -1,65 +1,33 @@
-# TODO: I haven't limited the number of processors yet- so this assumes we
-# have unlimited processors. Not a huge deal.
-
-
 #' Single sequential forks scheduler
-#'
-#' TODO: Inherit parameters from scheduleTaskList
 #'
 #' @export
 #' @inheritParams scheduleTaskList
-#' @param graph object of class \code{DependGraph} as returned from \code{\link{inferGraph}}
-#'  expression.
-#' @return schedule object of class \code{ForkSchedule}
+#' @return schedule \linkS4Class{ForkSchedule}
 scheduleFork = function(graph
-    , overhead = 1e3
-    , bandwidth = 1.5e9
 ){
 
-    nnodes = length(graph@code)
+}
 
-    might_fork = seq(nnodes)
-    might_fork = might_fork[time(graph) > overhead]
 
-    partialSchedule = data.frame(expression = seq(nnodes), fork = "run", time = time(graph))
+forkOne = function(node, allnodes, graph)
+{
+    p = partition(node_to_fork, graph)
+}
 
-    # I imagine this pattern generalizes to other greedy algorithms.
-    # But I'm not going to generalize it now.
-    for(i in seq_len(might_fork)){
-        reduction = sapply(might_fork, forkTimeReduction
-                       , partialSchedule = partialSchedule
-                       , graph = graph
-                       )
-        if(all(reduction < 0)){
-            break
-        }
-        node_to_fork = might_fork[which.max(reduction)]
-        might_fork = setdiff(might_fork, node_to_fork)
-        partialSchedule = scheduleOne(node_to_fork, partialSchedule, graph)
-    }
 
-    new("ForkSchedule", graph = graph
-            , fork = partialSchedule
-            , overhead = overhead
-            , bandwidth = bandwidth
+# Partition allnodes into ancestors, descendants, and independent with
+# respect to node
+partition = function(node, allnodes, graph)
+{
+    out = list(ancestors = ancestors(node, allnodes, graph)
+            , descendants = descendants(node, allnodes, graph)
             )
+    related = do.call(c, out)
+    out[["independent"]] = setdiff(allnodes, related)
+    out
 }
 
 
-# How long does the partial schedule take to complete if we fork one node?
-forkTimeReduction = function(node_to_fork, partialSchedule, graph)
-{
-    newSchedule = scheduleOne(node_to_fork, partialSchedule, graph)
-    runTime(partialSchedule) - runTime(newSchedule)
-}
-
-
-# How long does the partial schedule take to complete?
-runTime = function(partialSchedule)
-{
-    sum(partialSchedule[, "time"])
-}
-
-scheduleOne = function(node_to_fork, partialSchedule, graph)
+ancestors = function(node, allnodes, graph)
 {
 }
