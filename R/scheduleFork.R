8a2fee8b0492e562559777868efa53d065d5a7bf
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-09-13 15:22:10 -0700

    now Im thinking that the original implementation may be better

--- R/scheduleFork.R
+++ R/scheduleFork.R
@@ -2,39 +2,49 @@
 #'
 #' @export
 #' @inheritParams scheduleTaskList
+#' @param overhead seconds required to initialize a fork
 #' @return schedule \linkS4Class{ForkSchedule}
 scheduleFork = function(graph
+    , overhead = 1e-3
 ){
 
 }
 
 
-forkOne = function(node, allnodes, graph)
+forkOne = function(node, nodegroup, graph)
 {
-    p = partition(node_to_fork, graph)
+    p = partition(node, nodegroup, graph)
+    
 }
 
 
-# Partition allnodes into ancestors, descendants, and independent with
+# Partition nodegroup into ancestors, descendants, and independent with
 # respect to node
-partition = function(node, allnodes, graph)
+partition = function(node, nodegroup, graph)
 {
-    out = list(ancestors = familytree(node, "ancestors", allnodes, graph)
-            , descendants = familytree(node, "descendants", allnodes, graph)
+    out = list(ancestors = familytree(node, "ancestors", nodegroup, graph)
+            , descendants = familytree(node, "descendants", nodegroup, graph)
             )
     related = do.call(c, out)
-    out[["independent"]] = setdiff(allnodes, related)
+    out[["independent"]] = setdiff(nodegroup, related)
     out
 }
 
 
-# Recursively compute part of a family tree and intersect with allnodes
-familytree = function(node, direction, allnodes, graph)
+# Recursively compute part of a family tree and intersect with nodegroup
+familytree = function(node, direction, nodegroup, graph)
 {
     onegen = switch(direction, ancestors = predecessors, descendants = successors)
     g1 = onegen(node, graph)
-    g1 = intersect(g1, allnodes)
+    g1 = intersect(g1, nodegroup)
     g2plus = sapply(g1, familytree, direction = direction
-                     , allnodes = allnodes, graph = graph)
+                     , nodegroup = nodegroup, graph = graph)
     c(g1, g2plus)
 }
+
+
+
+# Unlist should do what I want, this produces 1:8
+#l = list(1:3, list(4, list(5, 6)), list(7), 8)
+#unlist(l)
+
