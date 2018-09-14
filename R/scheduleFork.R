4b84524112a9e11c9d6052660fb272142ae4813c
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-09-14 10:51:42 -0700

    3rd implementation of scheduleFork

--- R/scheduleFork.R
+++ R/scheduleFork.R
@@ -7,14 +7,52 @@
 scheduleFork = function(graph
     , overhead = 1e-3
 ){
+    nnodes = length(graph@code)
+    graphdf = graph@graph
 
+    # This algorithm works by continually removing elements from this
+    # vector.
+    might_fork = seq(nnodes)
+    might_fork = might_fork[time(graph) > overhead]
+
+    cur_schedule = seq(nnodes)
+
+    # I imagine this pattern generalizes to other greedy algorithms.
+    # But I'm not going to generalize it now.
+    for(i in seq_len(might_fork)){
+        forks = lapply(might_fork, forkOne
+                       , schedule = cur_schedule, graphdf = graphdf)
+
+        # Remove forked nodes that slow down the program.
+        reduction = sapply(forks, `[[`, "reduction")
+        slowdowns = reduction < 0
+        might_fork = setdiff(might_fork, mightfork[slowdowns])
+        if(length(might_fork) == 0)
+            break
+
+        # Greedy aspect- update the current schedule by choosing the node
+        # that most reduces program run time.
+        bestnode = forks[[which.max(reduction)]]
+        cur_schedule = bestnode[["schedule"]]
+
+        # Check if we're finished each time we update might_fork
+        might_fork = setdiff(might_fork, bestnode[["node"]])
+        if(length(might_fork) == 0)
+            break
+    }
+
+    cur_schedule
 }
 
 
-forkOne = function(node, nodegroup, graph)
+forkOne = function(node, schedule, graphdf)
 {
-    p = partition(node, nodegroup, graph)
+    p = partition(node, schedule, graphdf)
     
+    list(node = node
+         , schedule = new_schedule
+         , reduction = reduction
+         )
 }
 
 
@@ -41,10 +79,3 @@ familytree = function(node, direction, nodegroup, graph)
                      , nodegroup = nodegroup, graph = graph)
     c(g1, g2plus)
 }
-
-
-
-# Unlist should do what I want, this produces 1:8
-#l = list(1:3, list(4, list(5, 6)), list(7), 8)
-#unlist(l)
-
