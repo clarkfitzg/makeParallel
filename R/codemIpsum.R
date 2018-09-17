b2d3df69044211bc9dbdc2ed59e2ecd928360700
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-09-17 09:18:57 -0700

    idea for random code

---
+++ R/codemIpsum.R
@@ -0,0 +1,12 @@
+
+# Generate Random Code
+#
+# Random code would be useful in this package to test the correctness of
+# scheduling algorithms.
+#
+# Ethan talked about doing this. I wonder if we can put a probability
+# distribution on how scripts write code?
+#
+codemIpsum = function()
+{
+}
