f30aab66fbc9f43c23ae8e0a63e4ecb57fef3b29
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-11-19 17:25:12 -0800

    describe heuristic algorithm for finding what parts can be done in parallel

--- inst/experiments/fusion.R
+++ inst/experiments/fusion.R
@@ -7,10 +7,41 @@ g = inferGraph("vector_code.R")
 # These are lists, so I can add more info in here.
 g@graph$value
 
-vectorfuncs = c("<", "[", "exp")
+# I actually can't think of any others
+lhsVectorFuncs = c("[")
 
-# If the code was in this form I might be able to manipulate it more
-# easily.
+# There are many, many more
+rhsVectorFuncs = c("`<`", "`[`", "exp", "identity")
 
+# I can also do this more rigorously by specifying which arguments a
+# function is vectorized in and using `match.call`
 
-code = data.frame(lhs = 
+# Various forms might make the code easier to manipulate.
+# Here's a form that puts everything in terms of
+# lhs = func(args)
+# Assuming the code actually looks like this, or
+# lhs[ss] = func(args)
+
+code = list(lhs = lapply(g@code, `[[`, 2)
+    , func = lapply(g@code, `[[`, c(3, 1))
+    , args = lapply(g@code, function(e) as.list(e[[3]])[-1])
+    )
+
+code$vectorized = code$func %in% rhsVectorFuncs
+
+# Now we can fuse the vectorized functions based on the graph structure.
+# What exactly does this mean?
+# That we'll run all the parts in parallel that we possibly can before we
+# hit a general function call that forces us to collect the results from
+# the workers.
+# I think this means starting at a node, first the root in the graph, and
+# identifying all the children with vectorized functions that can run
+# before hitting a node with a general function call that forces you to
+# stop. 
+
+# To run, first run all the child nodes on each of the data chunks which
+# are distributed among the workers. Then have all the workers return their
+# part of the result to the manager process to evaluate the general
+# function call. Then we still have the data loaded on all the workers, so
+# we can continue to run the remainder of the program in this fork - join
+# manner, leaving as much of the data distributed as possible.
--- inst/experiments/vector_code.R
+++ inst/experiments/vector_code.R
@@ -2,7 +2,15 @@
 # Suppose x is a huge vector stored on disk, and we would like to
 # use it to compute y.
 
+x = readRDS("x.rds")
+
 cond = x < 0
 tmp = x[cond]
+
 x[cond] = exp(tmp)
-y = x
+
+#y = x # Making it follow the structure I want:
+#y = identity(x)
+
+# . represents no actual assignment here.
+. = plot(x)
