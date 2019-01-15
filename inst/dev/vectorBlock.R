302352794a6a98bb6029e755c36e226797b245f2
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-01-15 07:27:15 -0800

    experimenting with finding vector blocks

---
+++ inst/dev/vectorBlock.R
@@ -0,0 +1,40 @@
+library(makeParallel)
+
+# This is a list that says which parameters a function is vectorized in.
+vecfuncs = list(qnorm = c("p", "mean", "sd")
+                , exp = "x"
+                )
+
+# This will be easiest if we have the code in a particular form:
+# single lines of function calls with only named arguments.
+# No nesting.
+# This will be an issue with ... args, but we can deal with that later.
+
+code = parse(text = "
+x = seq(from = 0, to = 1, length.out = 100)
+y = qnorm(p = x)
+z = exp(x = y)
+result = sum(z)
+")
+
+g = inferGraph(code)
+e = g@code[[2]]
+
+# We basically want to follow the flow of the large vector x, and any derivative vectors, through the dependency graph.
+
+is_vectorized = function(e, .vecfuncs = vecfuncs)
+{
+    # Assuming it's the RHS of an expression.
+    call = e[[3]]
+    if(is.call(call)){
+        func_name = as.character(call[[1]])
+        func_name %in% names(.vecfuncs)
+    }
+}
+
+v = which(sapply(g@code, is_vectorized))
+gdf = g@graph
+vblock_condition = (gdf[, "from"] %in% v) & (gdf[, "to"] %in% v)
+
+vector_block_edges = gdf[vblock_condition, ]
+vector_block_edges
