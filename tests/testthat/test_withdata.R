ebf52a9136b418865ea41d61e1df3fa34698914a
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-04-02 09:34:59 -0700

    first test for using data descriptions

---
+++ tests/testthat/test_withdata.R
@@ -0,0 +1,29 @@
+test_that("simple case of input and output data descriptions", {
+
+    incode = parse(text = "
+        y = 2L * x
+    ", keep.source = FALSE)
+
+    xfile = temp.file()
+    yfile = temp.file()
+
+    xdata = 1:5
+    saveRDS(xdata, xfile)
+
+    xdescription = DataSource(symbol = "x", fun = readRDS, args = list(file = xfile))
+    ydescription = DataSink(symbol = "y", fun = saveRDS, args = list(file = yfile))
+
+    # Could also handle lists of data sources / sinks.
+    out = makeParallel(code, data = xdescription, save = ydescription)
+
+    outcode = writeCode(out)
+
+    eval(outcode)
+
+    y_actual = readRDS(yfile)
+
+    # symbols x, y should be available after we evaluate the code
+    expect_identical(y, y_actual, 2L * x)
+
+    unlink(c(xfile, yfile))
+}
