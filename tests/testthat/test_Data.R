ffc9d84a601618f288490e5094d72c1b1e8f155f
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-04-03 12:35:44 -0700

    idea to get around eval / deparse issue

--- tests/testthat/test_Data.R
+++ tests/testthat/test_Data.R
@@ -11,9 +11,14 @@ test_that("simple case of chunked input data descriptions", {
     saveRDS(6:10, xfile2)
 
     # What is bothering me?
-    # I'm going to have to evaluate and deparse the list of arguments to insert them into the script.
-    # This sounds dangerous.
+    # It's this- how am I going to insert the arguments into a generated script as code?
+   
+    # I could store the function arguments as R objects, save them alongside the written files,
+    # and then load and call them when I need them.
+    # Using functions in this way is more general than just assuming we have the full data loaded as R objects.
 
+    # I recall dask has a pretty elegant way of doing this, with tuples and function calls.
+ 
     xdescription = ChunkDataSource(fun = readRDS, args = list(xfile1, xfile2))
 
     out = makeParallel(incode, data = list(x = xdescription))
