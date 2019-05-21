ad34c7d8acaa41d75e9c0c62324f58c40bc5d790
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-21 15:17:22 -0700

    tests for column usage

--- tests/testthat/test_columnsUsed.R
+++ tests/testthat/test_columnsUsed.R
@@ -1,38 +1,69 @@
 # Doesn't (yet) handle:
 #
 # - created or updated columns
-#       dfname[, "col"] = bar()
-#       foo(dfname[, "col"])
-# - different object writes over dfname
-#       dfname = something_completely_different()
-#       foo(dfname[, "col"])
-# - when dfname is renamed
-#       df2 = dfname
+#       x[, "col"] = bar()
+#       foo(x[, "col"])
+# - different object writes over x
+#       x = something_completely_different()
+#       foo(x[, "col"])
+# - when x is renamed
+#       df2 = x
 #       foo(df2[, "col1"])
 # - NSE from `$`
-#       foo(dfname$col)
+#       foo(x$col)
 # - column names contained in a variable
 #       v = "col1"
 #       foo(df2[, v])
 # - 
 
-# Does handle:
-#
-# - single literal for `[`
-#       foo(dfname[, "col"])
-#       foo(dfname["col"])
-# - multiple literals for `[` combined with `c()`
-#       foo(dfname[, c("col1", "col2")])
-# - redefinitions based on columns. This is a special case when we can quit the analysis early
-#       dfname = dfname[, c("col1", "col2")]
-#
 test_that("single literal for `[[`", {
 
     e = parse(text = '
-        foo(dfname[["col"]])
+        foo(x[["col"]])
     ')
-
-    expect_equal(columnsUsed(e), "col")
+    expect_equal(columnsUsed(e, "x"), "col")
 })
 
 
+test_that("single literal for `[`", {
+
+    e1 = parse(text = '
+        foo(x[, "col"])
+    ')
+    expect_equal(columnsUsed(e1, "x"), "col")
+
+    e2 = parse(text = '
+        foo(x["col"])
+    ')
+    expect_equal(columnsUsed(e2, "x"), "col")
+
+})
+
+
+test_that("multiple literals for `[` combined with `c()`", {
+
+    e = parse(text = '
+       foo(x[, c("col1", "col2")])
+    ')
+    expect_equal(columnsUsed(e, "x"), c("col1", "col2"))
+})
+
+
+test_that("redefinitions based on columns", {
+# This is a special case when we can quit the analysis early.
+
+    e = parse(text = '
+       x = x[, c("col1", "col2")]
+       foo(x)
+    ')
+    expect_equal(columnsUsed(e, "x"), c("col1", "col2"))
+})
+
+
+test_that("undetermined usage", {
+
+    e = parse(text = '
+       foo(x)
+    ')
+    expect_null(columnsUsed(e, "x"))
+})
