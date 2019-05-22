5804efc7c1cab2cbc0a5ffeb2d413579792667e9
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-21 17:15:35 -0700

    getting started on columns used code

--- R/columnsUsed.R
+++ R/columnsUsed.R
@@ -1,15 +1,37 @@
 # TODO: Have CodeAnalysis::readFaster use this code.
 
 # find the names of all columns of the data frame `dfname` that code uses.
-# See tests for what it does and does not handle
-
+# See tests for what it currently does and does not handle
 columnsUsed = function(code, dfname)
 {
+
+    locations = find_var(code, dfname)
+    for(loc in locations){
+        value = oneUsage(loc, code)
+    }
+
+}
+
+
+oneUsage = function(loc, code, subset_funcs = c("[", "[["), assign_funcs = c("=", "<-"))
+{
+
+    ll = length(loc)
+    parent = code[[loc[-ll]]]
+    grandparent = code[[loc[-c(ll-1, ll)]]]
+    parent_func = as.character(parent[[1]])
+    grandparent_func = as.character(grandparent[[1]])
+
 }
 
 
 if(FALSE){
 
-    e = quote(x$foo)
+    find_var = makeParallel:::find_var
+    dfname = "x"
+    code = parse(text = "
+        foo(y)
+        bar(x[, 'col'])
+    ")
         
 }
