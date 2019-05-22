2102d7f06b5afb028c1bf44a9af785a30c2a2e83
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-21 18:16:33 -0700

    working through column code

--- R/columnsUsed.R
+++ R/columnsUsed.R
@@ -6,23 +6,74 @@ columnsUsed = function(code, dfname)
 {
 
     locations = find_var(code, dfname)
+    found = character()
     for(loc in locations){
-        value = oneUsage(loc, code)
+        r = oneUsage(loc, code, dfname)
+        cmd = r[["cmd"]]
+        if(cmd == "all_columns")
+            return(NULL)
+        found = c(found, r[["found"]])
+        if(cmd == "stop")
+            return(found)
+    }
+    found
+}
+
+
+# Return a list with the columns used and a command
+oneUsage = function(loc, code, dfname, subset_funcs = c("[", "[["), assign_funcs = c("=", "<-"))
+{
+    out = list(cmd = "continue", found = NULL)
+    ll = length(loc)
+
+    # Handle all possible cases
+
+    parent = code[[loc[-ll]]]
+    parent_func = as.character(parent[[1]])
+    if(!(parent_func %in% subset_funcs)){
+        # We don't know about this function, so assume the worst, that it uses all columns
+        out[["cmd"]] = "all_columns"
+        return(out)
+    }
+
+    colindex = switch(parent_func, "[" = 4L, "[[" = 3L)
+
+    colcode = parent[[colindex]]
+    if(is.character(colcode) || c_with_literals(colcode)){
+        # It's just a character vector, safe to evaluate
+        out[["found"]] = eval(colcode)
+    } else {
+        # It's not a simple character vector, so assume the worst, that it uses all columns
+        out[["cmd"]] = "all_columns"
+        return(out)
+    }
+
+    grandparent = code[[loc[-c(ll-1, ll)]]]
+    grandparent_func = as.character(grandparent[[1]])
+
+    # The special case when we reassign into the same variable
+    if(grandparent_func %in% assign_funcs){
+        varname = as.character(grandparent[[2L]])
+        if(varname == dfname){
+            out[["status"]] = "finished"
+        }
     }
 
 }
 
 
-oneUsage = function(loc, code, subset_funcs = c("[", "[["), assign_funcs = c("=", "<-"))
+# Check if expr creates a character vector from literals, meaning it looks like c("foo", "bar", ...)
+c_with_literals = function(expr)
 {
-
-    ll = length(loc)
-    parent = code[[loc[-ll]]]
-    grandparent = code[[loc[-c(ll-1, ll)]]]
-    parent_func = as.character(parent[[1]])
-    grandparent_func = as.character(grandparent[[1]])
-
+    if(as.character(expr[[1]]) != "c")
+        return(FALSE)
+    for(i in seq(2, length(expr))){
+        if(class(expr[[i]]) != "character")
+            return(FALSE)
+    }
+    TRUE
 }
+
 
 
 if(FALSE){
