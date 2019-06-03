00d8c9d2533e483c2a96d6d950c168ce48ddc3bd
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-06-03 08:53:58 -0700

    include temporary variables in target code

--- inst/pems/target_code.R
+++ inst/pems/target_code.R
@@ -14,21 +14,31 @@ npbin = function(...) "see pems.R"
 # 1. A split based on a data partition
 # 2. An lapply on the results of that split
 
-pems2_1 = read.csv(
-    pipe("cut -d , -f 2,6,7 stationID/313368.csv")
-    , col.names = c("station", "flow2", "occupancy2")
-    , colClasses = c("integer", "integer", "numeric")
-    )
-    
-pems2_2 = read.csv(
-    pipe("cut -d , -f 2,6,7 stationID/313369.csv")
-    , col.names = c("station", "flow2", "occupancy2")
-    , colClasses = c("integer", "integer", "numeric")
-    )
-    
-pems2_1 = split(pems_1, pems_1$station)
-pems2_2 = split(pems_2, pems_2$station)
+tmp0 = c("integer", "integer", "numeric")
+tmp1 = c("station", "flow2", "occupancy2")
 
+pems_1 = read.csv(
+    pipe("cut -d , -f 2,6,7 stationID/313368.csv")
+    , col.names = tmp1
+    , colClasses = tmp0
+    )
+    
+pems_2 = read.csv(
+    pipe("cut -d , -f 2,6,7 stationID/313369.csv")
+    , col.names = tmp1
+    , colClasses = tmp0
+    )
+
+pems_1 = pems_1[, tmp1]
+pems_2 = pems_2[, tmp1]
+
+tmp2_1 = pems_1[, "station"]
+tmp2_2 = pems_2[, "station"]
+
+pems2_1 = split(pems_1, tmp2_1)
+pems2_2 = split(pems_2, tmp2_2)
+
+# treat lapply as vectorized
 results_1 = lapply(pems2_1, npbin)
 results_2 = lapply(pems2_2, npbin)
 
