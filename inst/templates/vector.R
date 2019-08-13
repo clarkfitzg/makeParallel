8f9c12df529b8a0043ca76f6064a33212cde6fce
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-08-13 16:59:11 -0700

    unpacking code

--- inst/templates/vector.R
+++ inst/templates/vector.R
@@ -25,7 +25,16 @@ collected = clusterEvalQ(cls, {
     `_OBJECTS_RECEIVE_FROM_WORKERS`
 })
 
-# Unpack and assemble these objects
-# TODO:
+# Unpack and assemble the objects
+vars_to_collect = names(collected[[1]])
+for(i in seq_along(vars_to_collect)){
+    varname = vars_to_collect[i]
+    value = lapply(collected, `[[`, i)
+    # TODO: Generalize this to other combining functions besides c, rbind for data.frame
+    # For this we need to know if value is a data.frame
+    value = do.call(c, value)
+    assign(varname, value)
+}
+
 
 `_REMAINDER`
