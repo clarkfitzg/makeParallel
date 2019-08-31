be37052b96ed57ea79aee8d48858dc23982972b9
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-08-31 12:18:33 -0700

    use the directory name

--- tests/testthat/test_DataParallel.R
+++ tests/testthat/test_DataParallel.R
@@ -1,11 +1,12 @@
 # DataParallel has several tests which are contained in directories, but have very similar structure.
 # This function exercises these tests and integrates with testthtat.
-run_DataParallel = function(dir){
-    setwd(dir)
+run_DataParallel = function(path){
+    setwd(dirname(path))
     on.exit(setwd(".."))
-
-    source("transform.R")
+    source(path)
 }
 
 
-lapply(run_DataParallel, c("map_reduce_example", "two_blocks"))
+lapply(run_DataParallel, c("map_reduce_example/transform.R"
+                           , "two_blocks/transform.R"
+                           ))
