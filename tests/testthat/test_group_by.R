9a429a5df9cea192c23f38bdc724dcd6a78103ea
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-08-31 20:08:52 -0700

    test case for group by

---
+++ tests/testthat/test_group_by.R
@@ -0,0 +1,43 @@
+library(makeParallel)
+
+files = list.files("iris_csv", pattern = "*.csv", full.names = TRUE)
+
+# Can surely do this for the user
+sizes = file.info(files)[, "size"]
+
+x_desc = ChunkDataFiles(varName = "iris2"
+    , files = files
+	, sizes = sizes
+	, readFuncName = "read.csv"
+    , chunkClass = "data.frame"
+    )
+
+outFile = "gen/group_by.R"
+
+out = makeParallel("
+iris2split = split(iris2, iris2$Species)
+med_petal = sapply(iris2split, function(grp) median(grp$Petal.Length))
+saveRDS(med_petal, 'gen/med_petal.rds')
+"
+, data = x_desc
+, scheduler = scheduleDataParallel
+, platform = parallelLocalCluster()
+, chunkableFuncs = "sapply"
+, outFile = outFile
+, overWrite = TRUE
+)
+
+
+# Test code
+############################################################
+if(identical(Sys.getenv("TESTTHAT"), "true")){
+
+    rr = 'gen/med_petal.rds'
+    unlink(rr)
+    source(outFile)
+
+    result = readRDS(rr)
+    expected = readRDS("expected/med_petal.rds")
+    expect_equal(result, expected)
+
+}
