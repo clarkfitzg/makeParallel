4aa79ace4013c7e221d74b2cedecafc24716b901
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-09-10 19:29:23 -0700

    copy Duncan's data finding code over

--- tests/testthat/test_one_text_file.R
+++ tests/testthat/test_one_text_file.R
@@ -1,10 +1,12 @@
 # Testing the data source inference.
 
+library(makeParallel)
+
 out = makeParallel("
 dt = read.fwf('dates.txt', widths = 10)
 d = as.Date(vals[, 1])
 print(range(d))
-"
+", scheduler = scheduleDataParallel
 )
 
 
@@ -12,6 +14,6 @@ print(range(d))
 ############################################################
 if(identical(Sys.getenv("TESTTHAT"), "true")){
 
-    # 
+# Verify that the DataSource is correctly inferred.
 
 } 
