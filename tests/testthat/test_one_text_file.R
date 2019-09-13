8ef246e0becdc422f0ab8e8562e7b7dda4581bcc
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-09-12 17:17:37 -0700

    experiments with S3 in package code

--- tests/testthat/test_one_text_file.R
+++ tests/testthat/test_one_text_file.R
@@ -22,13 +22,6 @@ d1 = dataSource(quote(
     dt <- read.fwf('dates.txt', widths = 10L)
 ))
 
-inferDataSourceFromCall.read.csv_Call = function(expr, ...) "Boom!"
-
-tst = dataSource(quote(
-    dt <- read.csv('dates.txt', widths = 10L)
-))
-
-
 # Discovered in the original code by makeParallel
 d2 = dataSource(out)
 
@@ -37,3 +30,22 @@ expect_equal(d0, d1)
 expect_equal(d0, d2)
 
 } 
+
+
+if(FALSE){
+    # This is an extension mechanism, so could test it.
+    # Ugly though.
+    
+    inferDataSourceFromCall.read.csv_Call = function(expr, ...) "Boom!"
+
+    tst = dataSource(quote(
+        dt <- read.csv('dates.txt', widths = 10L)
+    ))
+
+    # This is what Duncan was talking about where you actually have to make the method available on the search path.
+    # The pacakage code cannot find this.
+    # Hence the need to make the function handler list available.
+
+    inferDataSourceFromCall.read.fwf_Call = function(expr, ...) "Boom Boom!"
+
+}
