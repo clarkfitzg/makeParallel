ebf319250ba81407b1bf8ea859d2fc4001d33ba1
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-09-15 14:25:00 -0700

    canonicalize ast

--- R/canonical_ast.R
+++ R/canonical_ast.R
@@ -1,7 +1,17 @@
-# 
+match_call_if_possible = function(node){
+    tryCatch(rstatic::match_call(node)
+             , error = function(e) node)
+}
+
+
 canonical_ast = function(code)
 {
-    rstatic::to_ast(code)
-    if(!is(ast, "Brace"))
-        stop("AST has unexpected form.")
+    ast = rstatic::to_ast(code)
+    if(!is(ast, "Brace")){
+        b = rstatic::Brace$new()
+        b$contents = ast
+        ast = b
+    }
+    rstatic::replace_nodes(ast, match_call_if_possible)
+    ast
 }
