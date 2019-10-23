bb33d16c6204363db5eb83ce2ced32a54e28d8da
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-10-23 11:03:31 -0700

    going to generate code with data.table, so removing disk based split implementation

--- R/generateDataParallel.R
+++ R/generateDataParallel.R
@@ -122,8 +122,29 @@ TEMPLATE_split_on_disk = quote({
 })
 
 
+TEMPLATE_UnixPlatform_DataLoadBlock_TextTableFiles = quote({
+    nlines = system2("wc", c("-l", `_DATA_FILE_NAME`), stdout = TRUE)
+    nlines = regmatches(nlines, regexpr("[0-9]+", nlines))
+    nlines = as.integer(nlines)
+    lines_per_file = ceiling(nlines / `_NWORKERS`)
+    chunk_file_dir = paste0("chunk_", `_DATA_FILE_NAME`)
+    dir.create(chunk_file_dir, showWarnings = FALSE)
+
+    system2("split", c("-l", lines_per_file, `_DATA_FILE_NAME`, paste0(chunk_file_dir, "/")))
+    chunk_files = list.files(chunk_file_dir, full.names = TRUE)
+
+    clusterExport(`_CLUSTER_NAME`, "chunk_files")
+
+    clusterEvalQ(`_CLUSTER_NAME`, {
+        read_arg = chunk_files[workerID]
+        `_DATA_VARNAME` = `_READ_FUNC`(read_arg)
+        NULL
+    })
+})
+
+
 setMethod("generate", signature(schedule = "DataLoadBlock", platform = "UnixPlatform", data = "TextTableFiles"),
-function(schedule, platform, data, template = TEMPLATE_split_on_disk, ...)
+function(schedule, platform, data, template = TEMPLATE_UnixPlatform_DataLoadBlock_TextTableFiles, ...)
 {
     if(length(data@files) == 1){
         # There's only a single text file, so split it up into as many text files as there are workers in the platform.
